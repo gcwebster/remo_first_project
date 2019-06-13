a344f2714b91259825a4e79ea5f8678bf9382fb9
george.websternationwide.co.uk <george.webster@nationwide.co.uk>
2019-06-13 13:42:41 +0100

    Everything

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import { greeting } from './greeting';
+import { Welcome } from './Welcome';
+import './styles/main.scss';
+
+console.log(greeting('George'));
+
+
+// function App() {
+//     return (
+//         <div>
+//             <p>HELLO</p>
+//             <Welcome />
+//         </div>
+//     );
+//     //console.log(greeting('George'));
+// }
+
+// ReactDOM.render(
+//     <App />,
+//     document.getElementById('root')
+// );
