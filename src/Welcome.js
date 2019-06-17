6a90aedc4780f8d7def1ea348cea7e802130ca14
george.websternationwide.co.uk <george.webster@nationwide.co.uk>
2019-06-17 13:42:57 +0100

    Bringing in a new babel plugin
    Allows easy image reading.

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,7 +1,12 @@
+import src from './images/Kilimanjaro2.jpg';
 class Welcome extends React.Component {
     render() {
         return (
-            <p>This is coming from inside the welcome component</p>
+            <div>
+                <p>This is coming from inside the welcome component</p>
+                <img src={src} />
+                <img src="./images/Kilimanjaro.jpg" />
+            </div>
         );
     }
 }
