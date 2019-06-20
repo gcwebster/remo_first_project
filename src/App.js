bf478234ce2bf3aceaf0498c0613c5cdbfd53534
george.websternationwide.co.uk <george.webster@nationwide.co.uk>
2019-06-20 11:20:08 +0100

    Created landing page and bought it into App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Greeting } from './Greeting'
 import { Welcome } from './Welcome'
 import { Counter } from './Counter'
+import { LandingPage } from './LandingPage'
 import './styles/main.scss'
 
 class App extends React.Component {
     render() {
         return (
             <div>
+                <LandingPage name={"George"} />
                 <p>This is coming from inside the app component</p>
                 <Greeting name="George" />
                 <Counter />
---
+++ src/LandingPage.js
@@ -0,0 +1,9 @@
+function LandingPage(props) {
+    return (
+        <div>
+            <h1>Hello, {props.name}</h1>
+        </div>
+    );
+}
+
+export { LandingPage }
