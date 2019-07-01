a81803a324543ca6307e154e1872b9f5cc69bfa9
george.websternationwide.co.uk <george.webster@nationwide.co.uk>
2019-07-01 15:30:29 +0100

    Helpful comment explaining how to use the checkbox component

--- src/formFull/FormInputElements/FormCheckboxInputElement.js
+++ src/formFull/FormInputElements/FormCheckboxInputElement.js
@@ -9,6 +9,13 @@ function FormCheckboxInputElement(props) {
 function displayCheckboxes(props) {
     var checkboxes = [];
     for (var i = 0; i < props.options.length; i++) {
+        /*
+        * An error will occur if you don't set the category to be empty on intial values in form
+        * This is because it can't search for an element of the category if the category doesn't yet exist
+        * e.g. hobbies would only be created once Kayaking (or other) was selected, meaning props.category.[props.options[i]] couldn't find anything
+        * Now it can go into the category since it exists (even if empty) and determine that the specific element doesn't exist (returns undefined instead of error)
+        */
+
         var title = props.category + "." + props.options[i];
         checkboxes.push(
             <div>
